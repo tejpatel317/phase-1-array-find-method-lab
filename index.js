7c2e82ab5f5267cd94bd097f1183303739805121
Tej Patel <tejpatel317@gmail.com>
2022-05-01 13:36:52 -0400

    commit tree 18a9279435914e27f35cb4c14711ec0b803bfe6c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    const x = record.find(callbackFunction)
+    return !!x ? x.year : undefined;
+}
+
+function callbackFunction(element, index, array) {
+    return (element.result === "W")
+}
